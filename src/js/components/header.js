3e2b91abb5089feeab427df705472abb2e0404a5
qualitat <developer.kosmos@gmail.com>
2020-02-14 17:51:42 +0300

    fixed mobile menu

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,13 +1,13 @@
 import BaseComponent from "./basecomponent";
-import MobileMenu    from "./mobilemenu";
-import AuthButton    from "./authbutton";
-import EVENTS        from "../events";
+import MobileMenu from "./mobilemenu";
+import AuthButton from "./authbutton";
+import EVENTS from "../events";
 import constants from "../constants";
 
 class Header extends BaseComponent {
   constructor(props) {
     super(props.element);
-    const popupMenuMobile = new MobileMenu({element: this.element.querySelector(constants.popupMenuMobile)});
+    const popupMenuMobile = new MobileMenu({ element: this.element.querySelector(constants.popupMenuMobile) });
     const menuMobileButton = this.element.querySelector(constants.menuMobileButton);
 
     menuMobileButton.addEventListener('click', function () {
@@ -15,12 +15,22 @@ class Header extends BaseComponent {
     });
 
     const authButtonElement = this.element.querySelector(constants.authButtonElement);
+    const authButtonElementMobile = this.element.querySelector(constants.authButtonElementMobile);
+    this.authButton = [];
     if (authButtonElement) {
-      this.authButton = new AuthButton({element: authButtonElement});
+      this.authButton.push(new AuthButton({ element: authButtonElement }));
+    }
+    if (authButtonElementMobile) {
+      this.authButton.push(new AuthButton({ element: authButtonElementMobile }));
     }
     this.savedNewsButton = this.element.querySelector(constants.savedNewsButton);
+    this.savedNewsButtonMobile = this.element.querySelector(constants.savedNewsButtonMobile);
     this.logoutButton = this.element.querySelector(constants.logoutButton);
+    this.logoutButtonMobile = this.element.querySelector(constants.logoutButtonMobile);
     this.logoutButton.addEventListener('click', function () {
+      document.dispatchEvent(new Event(EVENTS.logoutClicked));
+    });
+    this.logoutButtonMobile.addEventListener('click', function () {
       document.dispatchEvent(new Event(EVENTS.logoutClicked));
     });
     this.userNames = this.element.querySelectorAll(constants.userNames);
@@ -30,20 +40,20 @@ class Header extends BaseComponent {
     let isLoggedIn = props.isLoggedIn;
     let userName = props.userName;
     if (isLoggedIn) {
-      if (this.authButton) {
-        this.authButton.hide();
-      }
+      this.authButton.forEach(x => x.hide());
       this.savedNewsButton.classList.remove(constants.invisible);
+      this.savedNewsButtonMobile.classList.remove(constants.invisible);
       this.logoutButton.classList.remove(constants.invisible);
+      this.logoutButtonMobile.classList.remove(constants.invisible);
       for (let userNameTag of this.userNames) {
         userNameTag.textContent = userName;
       }
     } else {
-      if (this.authButton) {
-        this.authButton.show();
-      }
+      this.authButton.forEach(x => x.show());
       this.savedNewsButton.classList.add(constants.invisible);
+      this.savedNewsButtonMobile.classList.add(constants.invisible);
       this.logoutButton.classList.add(constants.invisible);
+      this.logoutButtonMobile.classList.add(constants.invisible);
     }
   }
 }
